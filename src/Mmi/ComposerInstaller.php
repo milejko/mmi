399d6aaeb3efec67b6966ec30ff68f76c60279f9
Mariusz Miłejko <mariusz@milejko.pl>
2015-09-22 13:34:53 +0200

    dist files

---
+++ src/Mmi/ComposerInstaller.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ * 
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2015 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+namespace Mmi;
+
+use Composer\Script\Event;
+
+/**
+ * Klasa używana przy instalacji composerem
+ */
+class ComposerInstaller {
+
+	/**
+	 * Pliki dystrybucyjne
+	 * @var array
+	 */
+	protected static $_distFiles = [
+		'dist-files' => '',
+	];
+
+	/**
+	 * Po aktualizacji
+	 * @param Event $event
+	 */
+	public static function postUpdate(Event $event) {
+		self::_initApp($event);
+		//wdrożenie bazy danych
+		(new \Mmi\Db\Deployer())->deploy();
+	}
+
+	/**
+	 * Po instalacji
+	 * @param Event $event
+	 */
+	public static function postInstall(Event $event) {
+		self::_initApp($event);
+		//wdrożenie bazy danych
+		(new \Mmi\Db\Deployer())->deploy();
+	}
+
+	/**
+	 * Inicjalizacja autoloadera
+	 * @param Event $event
+	 */
+	protected static function _initApp(Event $event) {
+		//określenie katalogu vendorów
+		$vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+		//ustawianie ścieżki bazowej projektu
+		define('BASE_PATH', $vendorDir . '/../');
+		//kopiowanie plików dist
+		self::_copyDistFiles();
+		//wczytanie autoloadera
+		require $vendorDir . '/autoload.php';
+		//powołanie aplikacji
+		$application = new \Mmi\App\Kernel('\Mmi\App\BootstrapCli');
+		//ustawienie typu odpowiedzi na plain
+		\Mmi\App\FrontController::getInstance()->getResponse()->setTypePlain();
+		//uruchomienie aplikacji
+		$application->run();
+	}
+
+	/**
+	 * Kopiuje pliki z dystrybucji
+	 */
+	protected static function _copyDistFiles() {
+		//iteracja po wymaganych plikach
+		foreach (self::$_distFiles as $src => $dest) {
+			//kopiowanie pojedynczego pliku
+			if (!file_exists(file_exists(BASE_PATH . $dest)) && is_file(BASE_PATH . $src)) {
+				copy(BASE_PATH . $src, BASE_PATH . $dest);
+				continue;
+			}
+			//kopiowanie katalogów
+			if (is_dir(BASE_PATH . $src)) {
+				//kopiowanie rekursywne
+				\Mmi\FileSystem::copyRecursive(BASE_PATH . $src, BASE_PATH . $dest);
+			}
+		}
+	}
+
+}
