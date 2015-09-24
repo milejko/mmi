00d5860dfe7de67f3531e0f98df33976deaac7ea
Mariusz Miłejko <mariusz@milejko.pl>
2015-09-24 13:36:53 +0200

    fix literówek

--- src/Mmi/Convert/Base58.php
+++ src/Mmi/Convert/Base58.php
@@ -7,7 +7,7 @@
  * @copyright  Copyright (c) 2010-2015 Mariusz Miłejko (http://milejko.com)
  * @license    http://milejko.com/new-bsd.txt New BSD License
  */
-namespace \Mmi\Convert;
+namespace Mmi\Convert;
 
 /**
  * Klasa liczb podstawy 58
