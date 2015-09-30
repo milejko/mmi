c97e50d4b9d62721e46cf4a41678dfbf417af108
Mariusz Miłejko <mariusz@milejko.pl>
2015-09-30 23:21:20 +0200

    Refactor

---
+++ src/Mmi/Db/Adapter/PdoBindHelper.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ * 
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2015 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+namespace Mmi\Db\Adapter;
+
+/**
+ * Klasa generująca kolejne klucze bind
+ */
+class PdoBindHelper {
+
+	/**
+	 * Indeks powiązań
+	 * @var integer
+	 */
+	private static $_bindIndex = 1000000;
+
+	/**
+	 * Zwraca kolejny klucz do binda
+	 * @return string
+	 */
+	public static function generateBindKey() {
+		//generowanie kolejnego klucza bind
+		return str_replace([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], self::$_bindIndex++
+		);
+	}
+
+}
