2955ca39c8c89eff8dc17229653a0c32802d87b4
Mariusz Miłejko <mariusz@milejko.pl>
2015-10-16 21:02:19 +0200

    Simplified config

--- src/Mmi/App/KernelConfig.php
+++ src/Mmi/App/KernelConfig.php
@@ -95,20 +95,4 @@ abstract class KernelConfig {
 	 */
 	public $host = 'localhost';
 
-	/**
-	 * Konstruktor konfiguracji
-	 */
-	public function __construct() {
-		//konfiguracja bufora
-		$this->cache = new \Mmi\Cache\CacheConfig;
-		//konfiguracja bazy danych
-		$this->db = new \Mmi\Db\DbConfig;
-		//ustawienia loggera
-		$this->log = new \Mmi\Log\LogConfig;
-		//ustawienia routera
-		$this->router = new \Mmi\Mvc\RouterConfig;
-		//konfiguracja sesji
-		$this->session = new \Mmi\Session\SessionConfig;
-	}
-
 }
