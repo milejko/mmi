f3c4cdb2b31299a4d5667201de4f5956a7d9dc74
Maciej Pawłowski <mac@macu.eu>
2016-02-01 11:17:21 +0100

    wysyłka parametrów jako array o jednym zagnieżdżeniu

--- src/Mmi/JsonRpc/JsonClient.php
+++ src/Mmi/JsonRpc/JsonClient.php
@@ -70,7 +70,7 @@ class JsonClient {
 		$request = new \Mmi\JsonRpc\JsonRequest;
 		$request->jsonrpc = '2.0';
 		$request->method = $method;
-		$request->params = ($params instanceof \Mmi\Model\Dto) ? array_values($params->toArray()) : array_values($params);
+		$request->params = (count($params) == 1 && is_array($params[0])) ? $params[0] : array_values($params);
 		$request->id = $id;
 		
 		//pobieranie odpowiedzi z serwera
