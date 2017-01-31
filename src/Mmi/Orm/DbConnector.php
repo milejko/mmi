525c4d018a2c227db05e8ef9712188ef0ddd2ce1
Mariusz Miłejko <mariusz@milejko.pl>
2017-01-31 14:55:01 +0100

    critical exception fix (when table not in db)

--- src/Mmi/Orm/DbConnector.php
+++ src/Mmi/Orm/DbConnector.php
@@ -109,10 +109,7 @@ class DbConnector {
 			return $structure;
 		}
 		//pobranie z adaptera
-		if ([] === $structure = static::getAdapter()->tableInfo($tableName)) {
-			//brak tabeli
-			throw new \Mmi\App\KernelException('Table not found ' . $tableName . ', check if database is properly deployed');
-		}
+		$structure = static::getAdapter()->tableInfo($tableName);
 		//zapis do bufora
 		if (static::$_cache !== null) {
 			static::$_cache->save($structure, $cacheKey, 28800);
