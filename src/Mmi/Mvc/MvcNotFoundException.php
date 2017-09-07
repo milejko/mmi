096e01d52db46146e96b06135626d532b1f8e751
Maciej Pawłowski <mac@macu.eu>
2017-09-07 20:40:41 +0200

    newrelic ignore transaction

--- src/Mmi/Mvc/MvcNotFoundException.php
+++ src/Mmi/Mvc/MvcNotFoundException.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Mmi Framework (https://code.google.com/p/mmicms/)
- * 
- * @link       https://code.google.com/p/mmicms/
- * @copyright  Copyright (c) 2010-2014 Mariusz Miłejko (http://milejko.com)
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ *
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2016 Mariusz Miłejko (http://milejko.com)
  * @license    http://milejko.com/new-bsd.txt New BSD License
  */
 
@@ -13,14 +13,28 @@ namespace Mmi\Mvc;
 use Monolog\Logger;
 
 /**
- * Klasa wyjątku nieodnalezionego kontrolera
+ * Klasa wyjątku nieodnalezionego miejsca mvc
  */
-class MvcNotFoundException extends MvcException {
+class MvcNotFoundException extends MvcException
+{
 
 	/**
 	 * Poziom logowania
 	 * @var integer
 	 */
 	protected $code = Logger::INFO;
+
+	/**
+	 * Konstruktor
+	 * @param string $message
+	 * @param integer $code
+	 * @param \Exception $previous
+	 */
+	public function __construct($message = "", $code = 0, \Exception $previous = null)
+	{
+		//ignorowanie transakcji
+		extension_loaded('newrelic') ? newrelic_ignore_transaction() : null;
+		parent::__construct($message, $code, $previous);
+	}
 
 }
