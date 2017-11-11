57d5ec747fcde30add8881a6e8f3ccb897d8e76f
Dawid Federowicz <d.federowicz@nowaera.pl>
2017-11-11 13:04:30 +0100

    PHPUnit for Mmi/Image/Exif.php update

--- tests/Mmi/Image/ExifTest.php
+++ tests/Mmi/Image/ExifTest.php
@@ -17,14 +17,17 @@ use Mmi\Image\Exif;
 class ExifTest extends \PHPUnit\Framework\TestCase
 {
 
-    CONST imagePath = BASE_PATH . '/tests/data/test2.jpeg';
+    CONST imagePath1 = BASE_PATH . '/tests/data/test1.jpeg';
+    CONST imagePath2 = BASE_PATH . '/tests/data/test2.jpeg';
     
-    private $_img;
+    private $_img1;
+    private $_img2;
     
     public function setUp()
     {
         try {
-            $this->_img = new Exif(self::imagePath);
+            $this->_img1 = new Exif(self::imagePath1);
+            $this->_img2 = new Exif(self::imagePath2);
         } catch(\Exception $ex) {
             return $ex->getMessage();
         }
@@ -32,61 +35,73 @@ class ExifTest extends \PHPUnit\Framework\TestCase
     
     public function testGetIsoSpeed()
     {
-        $this->assertEquals(null, $this->_img->getIsoSpeed());
+        $this->assertEquals(null, $this->_img1->getIsoSpeed());
+        $this->assertEquals('100', $this->_img2->getIsoSpeed());
     }
 
     public function testGetCamera()
     {
-        $this->assertEquals('EASTMAN KODAK COMPANY', $this->_img->getCamera());
+        $this->assertEquals('EASTMAN KODAK COMPANY', $this->_img1->getCamera());
+        $this->assertEquals('Hasselblad', $this->_img2->getCamera());
     }
     
     public function testGetCameraMake()
     {
-        $this->assertEquals('EASTMAN KODAK COMPANY', $this->_img->getCameraMake());
+        $this->assertEquals('EASTMAN KODAK COMPANY', $this->_img1->getCameraMake());
+        $this->assertEquals('Hasselblad', $this->_img2->getCameraMake());
     }
     
     public function testGetCameraModel()
     {
-        $this->assertEquals(null, $this->_img->getCameraModel());
+        $this->assertEquals(null, $this->_img1->getCameraModel());
+        $this->assertEquals(null, $this->_img2->getCameraModel());
     }
     
     public function testGetExposureTime()
     {
-        $this->assertEquals('1/8', $this->_img->getExposureTime());
+        $this->assertEquals('1/8', $this->_img1->getExposureTime());
+        $this->assertEquals('1/500', $this->_img2->getExposureTime());
     }
     
     public function testGetAperture()
     {
-        $this->assertEquals('2.7999999999999998', $this->_img->getAperture());
+        $this->assertEquals('2.7999999999999998', $this->_img1->getAperture());
+        $this->assertEquals('9.5', $this->_img2->getAperture());
     }
 
     public function testGetCreationDate()
     {
-        $this->assertEquals('2004-04-10 20:38:21', $this->_img->getCreationDate());
+        $this->assertEquals('2004-04-10 20:38:21', $this->_img1->getCreationDate());
+        $this->assertEquals('2016-10-07 11:51:32', $this->_img2->getCreationDate());
     }
     
     public function testGetWidth()
     {
-        $this->assertEquals('1350.0', $this->_img->getWidth());
+        $this->assertEquals('1350.0', $this->_img1->getWidth());
+        $this->assertEquals('8272.0', $this->_img2->getWidth());
     }
     
     public function testGetHeight()
     {
-        $this->assertEquals('900.0', $this->_img->getHeight());
+        $this->assertEquals('900.0', $this->_img1->getHeight());
+        $this->assertEquals('6200.0', $this->_img2->getHeight());
     }
     
     public function testGetOrientation()
     {
-        $this->assertEquals('1', $this->_img->getOrientation());
+        $this->assertEquals('1', $this->_img1->getOrientation());
+        $this->assertEquals('1', $this->_img2->getOrientation());
     }
     
     public function testGetLongitude()
     {
-        $this->assertEquals(null, $this->_img->getLongitude());
+        $this->assertEquals(null, $this->_img1->getLongitude());
+        $this->assertEquals(null, $this->_img2->getLongitude());
     }
     
     public function testGetLatitude()
     {
-        $this->assertEquals(null, $this->_img->getLatitude());
+        $this->assertEquals(null, $this->_img1->getLatitude());
+        $this->assertEquals(null, $this->_img2->getLatitude());
     }
 }
--- tests/Mmi/Image/ImageTest.php
+++ tests/Mmi/Image/ImageTest.php
@@ -8,209 +8,81 @@
  * @license    https://en.wikipedia.org/wiki/BSD_licenses New BSD License
  */
 
-namespace Mmi\Image;
+namespace Mmi\Test\Image;
 
+use Mmi\Image\Image;
 /**
- * Klasa obróbki obrazów
+ * Test klasy obróbki obrazów
  */
-class Image
+class ImageTest extends \PHPUnit\Framework\TestCase
 {
 
-    /**
-     * Konwertuje string, lub binaria do zasobu GD
-     * @param mixed $input
-     * @return resource
-     */
-    public static function inputToResource($input)
+    CONST imagePath = BASE_PATH . '/tests/data/test.png';
+    
+    private $_img;
+    private $_imagePath;
+    
+    public function setUp()
     {
+        try {
+            $this->_img = new Image();
+            $this->_imagePath = self::imagePath;
+        } catch(\Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+    
+    
+    public function testInputToResource()
+    {
+        $this->assertTrue(file_exists($this->_imagePath), 'No file');
+        $this->assertEquals('resource', gettype($this->_img->inputToResource($this->_imagePath)));
         //jeśli resource zwrot
-        if (gettype($input) == 'resource') {
-            return $input;
-        }
+//        if (gettype($input) == 'resource') {
+//            return $input;
+//        }
         //jeśli krótki content zakłada że to ścieżka pliku
-        return imagecreatefromstring((strlen($input) < 1024) ? file_get_contents($input) : $input);
+//        return imagecreatefromstring((strlen($input) < 1024) ? file_get_contents($input) : $input);
+    }
+    
+    public function testScaleCrop()
+    {
+        $x = rand(1, 100);
+        $y = rand(1, 100);
+        $this->assertEquals('aaa', $this->_img->scale($this->_imagePath, $x, $y));
+    }
+    
+    public function testScaleProportional()
+    {
+        $percent = rand(1, 100);
+        $this->assertTrue($this->_img->scaleProportional($this->_imagePath, $percent), 'Obrazek przeskalowany o '.$percent.'%');
+    }
+    
+    public function testScale()
+    {
+        
+    }
+    
+    public function testScalex()
+    {
+        
+    }
+    
+    public function testScaley()
+    {
+        
+    }
+    
+    public function testScaleMax()
+    {
+        
+    }
+    
+    public function testCrop()
+    {
+        
     }
 
-    /**
-     * Skaluje i przycina obrazek tak aby pasował do podanych wymiarów, zachowuje proporcje
-     * @param mixed $input wejście
-     * @param int $x wysokość do której chcemy przeskalować obrazek
-     * @param int $y szerokość do której chcemy przeskalować obrazek
-     * @return resource obrazek
-     */
-    public static function scaleCrop($input, $x, $y)
-    {
-        //brak zasobu
-        if (!($input = self::inputToResource($input))) {
-            return;
-        }
-        //badanie rozmiaru obrazu
-        $width = imagesx($input);
-        $height = imagesy($input);
-        //obliczanie skali
-        $scale = max($y / $height, $x / $width);
-        //obliczanie zeskalowanych wymiarów
-        $sx = round($width * $scale);
-        $sy = round($height * $scale);
-        //cropowanie zeskalowanego obrazu
-        return self::crop(self::scale($input, $sx, $sy), abs($sx - $x) / 2, abs($sy - $y) / 2, $x, $y);
-    }
-
-    /**
-     * Skaluje obrazek o podany procent zachowując proporcje
-     * @param mixed $input wejście
-     * @param int $percent procent o jaki ma być zmieniony rozmiar obrazka
-     * @return resource obrazek
-     */
-    public static function scaleProportional($input, $percent)
-    {
-        //brak zasobu
-        if (!($input = self::inputToResource($input))) {
-            return;
-        }
-        //badanie rozmiarów obrazu
-        $width = imagesx($input);
-        $height = imagesy($input);
-        //obliczanie rozmiarów po skalowaniu
-        $sx = round($width * $percent / 100);
-        $sy = round($height * $percent / 100);
-        //tworzenie tymczasowego zasobu
-        $tmp = imagecreatetruecolor($sx, $sy);
-        //zapis przeźroczystości
-        self::_saveAlpha($tmp);
-        //skalowanie
-        imagecopyresampled($tmp, $input, 0, 0, 0, 0, $sx, $sy, $width, $height);
-        return $tmp;
-    }
-
-    /**
-     * Skaluje obrazek proporcjonalnie do podanych maksymalnych wymiarów
-     * @param mixed $input wejście
-     * @param int $maxDimX wysokość do której chcemy przeskalować obrazek
-     * @param int $maxDimY szerokość do której chcemy przeskalować obrazek
-     * @return resource obrazek
-     */
-    public static function scale($input, $maxDimX, $maxDimY = null)
-    {
-        //brak zasobu
-        if (!($input = self::inputToResource($input))) {
-            return;
-        }
-        //badanie rozmiaru obrazu
-        $width = imagesx($input);
-        $height = imagesy($input);
-        //nie podano wysokości
-        if (null === $maxDimY) {
-            //obraz jest szerszy niż wyższy
-            if ($width > $height) {
-                //skalowanie do maksymalnego X-a
-                return self::scalex($input, $maxDimX);
-            }
-            //obraz jest wyższy niż szerszy, skalowanie do maksymalnego Y-ka
-            return self::scaley($input, $maxDimX);
-        }
-        //obliczanie współczynników
-        $ratioX = $maxDimX / $width;
-        $ratioY = $maxDimY / $height;
-        //skalowanie proporcjonalne ze współczynnikiem X
-        if ($ratioX < $ratioY && $ratioX < 1) {
-            return self::scaleProportional($input, $ratioX * 100);
-        }
-        //skalowanie proporcjonalne ze współczynnikiem Y
-        if ($ratioY <= $ratioX && $ratioY < 1) {
-            return self::scaleProportional($input, $ratioY * 100);
-        }
-        //zwrot obrazu
-        return $input;
-    }
-
-    /**
-     * Pomniejsza obrazek do danej szerokości zachowując proporcje, nie powiększa obrazka.
-     * @param mixed $input wejście
-     * @param int $maxDim szerokość do której chcemy pomniejszyć obrazek
-     * @return resource obrazek
-     */
-    public static function scalex($input, $maxDim)
-    {
-        return self::scaleMax($input, $maxDim);
-    }
-
-    /**
-     * Pomniejsza obrazek do danej wysokości zachowując proporcje, nie powiększa obrazka.
-     * @param mixed $input wejście
-     * @param int $maxDim wysokość do której chcemy pomniejszyć obrazek
-     * @return resource obrazek
-     */
-    public static function scaley($input, $maxDim)
-    {
-        return self::scaleMax($input, $maxDim, false);
-    }
-
-    /**
-     * Pomniejsza obrazek do maksymalnej długości lub szerokości (proporcjonalnie)
-     * @param mixed $input wejście
-     * @param int $maxDim wysokość do której chcemy pomniejszyć obrazek
-     * @param boolean $horizontal
-     * @return resource obrazek
-     */
-    public static function scaleMax($input, $maxDim, $horizontal = true)
-    {
-        //brak zasobu
-        if (!($input = self::inputToResource($input))) {
-            return;
-        }
-        //badanie rozmiarów obrazu
-        $width = imagesx($input);
-        $height = imagesy($input);
-        //obraz jest już prawidłowy
-        if (($horizontal ? $width : $height) <= $maxDim) {
-            return $input;
-        }
-        //obliczanie proporcji i skalowanie proporcjonalne
-        return self::scaleProportional($input, ($maxDim / ($horizontal ? $width : $height)) * 100);
-    }
-
-    /**
-     * Wycina fragment obrazka z punktu x,y o danej długości i wysokości
-     * @param mixed $input wejście
-     * @param int $x współrzędna x
-     * @param int $y współrzędna y
-     * @param int $newWidth nowa Szerokość
-     * @param int $newHeight nowa Wysokość
-     * @return resource obrazek
-     */
-    public static function crop($input, $x, $y, $newWidth, $newHeight)
-    {
-        //brak zasobu
-        if (!($input = self::inputToResource($input))) {
-            return;
-        }
-        //obliczanie nowej szerokości
-        if (($sx = imagesx($input)) < $newWidth + $x) {
-            $newWidth = ($sx - $x) > 0 ? ($sx - $x) : 1;
-        }
-        //obliczanie nowej wysokości
-        if (($sy = imagesy($input)) < $newHeight + $y) {
-            $newHeight = ($sy - $y) > 0 ? ($sy - $y) : 1;
-        }
-        //wycinanie obrazka
-        $destination = imagecreatetruecolor($newWidth, $newHeight);
-        //zapis przeźroczystości
-        self::_saveAlpha($destination);
-        //przycinanie
-        imagecopy($destination, $input, 0, 0, $x, $y, $newWidth, $newHeight);
-        //zwrot
-        return $destination;
-    }
-
-    /**
-     * Zachowanie alphy
-     * @param resource $imgRes
-     */
-    protected static function _saveAlpha($imgRes)
-    {
-        imagealphablending($imgRes, false);
-        imagesavealpha($imgRes, true);
-    }
+    
 
 }
