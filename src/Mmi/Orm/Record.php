8f29921eee838b18cdb5871f82bf4c78e679ce6d
Mariusz Miłejko <mariusz@milejko.pl>
2017-11-28 15:20:36 +0100

    record fix updating id (deletions)

--- src/Mmi/Orm/Record.php
+++ src/Mmi/Orm/Record.php
@@ -41,7 +41,7 @@ class Record extends \Mmi\Orm\RecordRo
         }
         $query = $this->_queryClass;
         $bindKey = PdoBindHelper::generateBindKey();
-        $result = DbConnector::getAdapter()->delete((new $query)->getTableName(), $this->_pkWhere($bindKey), [$bindKey => $this->getPk()]);
+        $result = DbConnector::getAdapter()->delete((new $query)->getTableName(), $this->_pkWhere($bindKey), [$bindKey => $this->getInitialStateValue('id')]);
         return ($result > 0) ? true : false;
     }
 
