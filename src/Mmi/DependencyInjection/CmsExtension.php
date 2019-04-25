32b37572586e89b79f89c8979384376e40c5aa61
Marcin Bonk <m.bonk@nowaera.pl>
2019-04-25 13:47:19 +0200

    Up

--- src/Mmi/DependencyInjection/CmsExtension.php
+++ src/Mmi/DependencyInjection/CmsExtension.php
@@ -22,7 +22,5 @@ class CmsExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__) . '/Resources/config'));
-        $loader->load('services.xml');
-        $loader->load('web.xml');
     }
 }
