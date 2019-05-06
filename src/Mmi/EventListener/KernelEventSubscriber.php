db7a513fd193a4749959fb9874aeec518d5e2795
Marcin Bonk <m.bonk@nowaera.pl>
2019-05-06 14:38:56 +0200

    Session warmer

--- src/Mmi/EventListener/KernelEventSubscriber.php
+++ src/Mmi/EventListener/KernelEventSubscriber.php
@@ -4,14 +4,13 @@ namespace Mmi\EventListener;
 
 use Mmi\Session\Session;
 use Mmi\Session\SessionConfig;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 /**
  * Class KernelEventSubscriber
  * @package Mmi\EventListener
  */
-class KernelEventSubscriber implements EventSubscriberInterface
+class KernelRequestSessionWarmer
 {
     /**
      * @var Session
@@ -26,33 +25,6 @@ class KernelEventSubscriber implements EventSubscriberInterface
     public function __construct(Session $session)
     {
         $this->session = $session;
-    }
-    
-    /**
-     * Returns an array of event names this subscriber wants to listen to.
-     *
-     * The array keys are event names and the value can be:
-     *
-     *  * The method name to call (priority defaults to 0)
-     *  * An array composed of the method name to call and the priority
-     *  * An array of arrays composed of the method names to call and respective
-     *    priorities, or 0 if unset
-     *
-     * For instance:
-     *
-     *  * ['eventName' => 'methodName']
-     *  * ['eventName' => ['methodName', $priority]]
-     *  * ['eventName' => [['methodName1', $priority], ['methodName2']]]
-     *
-     * @return array The event names to listen to
-     */
-    public static function getSubscribedEvents()
-    {
-        return [
-            'kernel.request' => [
-                ['warmUpSession']
-            ]
-        ];
     }
     
     /**
