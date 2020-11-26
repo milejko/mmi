68dd4bc4547d5db16e7166a11c29c0d53f9a5ca6
Mariusz <m.jarzab@nowaera.pl>
2020-11-26 17:13:36 +0100

    3.9.5.2 redis delete deprecated

--- src/Mmi/Cache/RedisHandler.php
+++ src/Mmi/Cache/RedisHandler.php
@@ -2,7 +2,7 @@
 
 /**
  * Mmi Framework (https://github.com/milejko/mmi.git)
- * 
+ *
  * @link       https://github.com/milejko/mmi.git
  * @copyright  Copyright (c) 2010-2017 Mariusz Miłejko (mariusz@milejko.pl)
  * @license    https://en.wikipedia.org/wiki/BSD_licenses New BSD License
@@ -98,7 +98,7 @@ class RedisHandler implements CacheHandlerInterface
      */
     public function delete($key)
     {
-        $this->_server->delete($this->_namespace . '_' . $key);
+        $this->_server->del($this->_namespace . '_' . $key);
         return true;
     }
 
