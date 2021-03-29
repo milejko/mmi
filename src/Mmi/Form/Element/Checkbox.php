6acfc08c0e350da10deb4ee5388d082f6a923ca4
telemetricsystems <robert@telemetric.pl>
2021-03-29 12:34:45 +0200

    Modify checkbox values (#31)
    Co-authored-by: Robert Pawłowski <r.pawlowski@nowaera.pl>

--- src/Mmi/Form/Element/Checkbox.php
+++ src/Mmi/Form/Element/Checkbox.php
@@ -2,7 +2,7 @@
 
 /**
  * Mmi Framework (https://github.com/milejko/mmi.git)
- * 
+ *
  * @link       https://github.com/milejko/mmi.git
  * @copyright  Copyright (c) 2010-2017 Mariusz Miłejko (mariusz@milejko.pl)
  * @license    https://en.wikipedia.org/wiki/BSD_licenses New BSD License
@@ -28,7 +28,7 @@ class Checkbox extends ElementAbstract
         parent::__construct($name);
         $this->setRenderingOrder(['fetchBegin', 'fetchField', 'fetchLabel', 'fetchDescription', 'fetchErrors', 'fetchEnd'])
             ->setLabelPostfix('')
-            ->setValue(1);
+            ->setValue(true);
     }
 
     /**
