3194256840af1b9d91591ea50b35284c35161759
Mariusz Miłejko <mariusz@milejko.pl>
2021-07-21 17:14:16 +0200

    db tests

--- tests/Mmi/Db/DbProfilerTest.php
+++ tests/Mmi/Db/DbProfilerTest.php
@@ -10,6 +10,7 @@
 
 namespace Mmi\Test\Db;
 
+use Mmi\App\AppProfiler;
 use Mmi\Db\DbProfiler;
 
 /**
@@ -20,23 +21,23 @@ class DbProfilerTest extends \PHPUnit\Framework\TestCase
 
     public function testCount()
     {
-        $profiler = new DbProfiler;
+        $profiler = new DbProfiler(new AppProfiler);
         $this->assertEquals(0, $profiler->count());
-        $this->assertInstanceOf('\Mmi\Db\DbProfiler', $profiler->event(new \PDOStatement, ['test' => 'test'], 0));
+        $this->assertNull($profiler->event(new \PDOStatement, ['test' => 'test'], 0));
         $this->assertEquals(1, $profiler->count());
         $this->assertEquals(0, $profiler->elapsed());
         $this->assertCount(1, $profiler->get());
-        $this->assertInstanceOf('\Mmi\Db\DbProfiler', $profiler->event(new \PDOStatement, [1], 31));
+        $this->assertNull($profiler->event(new \PDOStatement, [1], 31));
         $this->assertEquals(2, $profiler->count());
         $this->assertGreaterThan(30, $profiler->elapsed());
-        $this->assertInstanceOf('\Mmi\Db\DbProfiler', $profiler->event(new \PDOStatement, [1], 31));
-        $this->assertInstanceOf('\Mmi\Db\DbProfiler', $profiler->event(new \PDOStatement, [1], 31));
+        $this->assertNull($profiler->event(new \PDOStatement, [1], 31));
+        $this->assertNull($profiler->event(new \PDOStatement, [1], 31));
         $this->assertCount(4, $profiler->get());
     }
 
     public function testGet()
     {
-        $this->assertEmpty((new DbProfiler)->get());
+        $this->assertEmpty((new DbProfiler(new AppProfiler))->get());
     }
 
 }
