7d9842e4e7b31f63fe4af388cc28e5b137811602
pgalazka <patryk.e.galazka@gmail.com>
2021-10-26 21:17:24 +0200

    phone validator + multifield dynamic validation fix

---
+++ src/Mmi/Validator/Phone.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ *
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2017 Mariusz Miłejko (mariusz@milejko.pl)
+ * @license    https://en.wikipedia.org/wiki/BSD_licenses New BSD License
+ */
+
+namespace Mmi\Validator;
+
+/**
+ * Walidator adresu email
+ */
+class Phone extends ValidatorAbstract
+{
+
+    /**
+     * Komunikat błędu
+     */
+    const INVALID = 'validator.phone.message';
+
+    /**
+     * Sprawdza czy tekst jest numerem telfonu
+     * @param string $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        //dopasowanie telefonu
+        if (preg_match('/([0-9\+])([0-9\+ \(\)]+)/i', $value)) {
+            return true;
+        }
+        return $this->_error(static::INVALID);
+    }
+
+}
