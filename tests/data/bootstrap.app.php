933394ab03bade6e1d1493d32958fcbff7125fb0
Wojciech Miedzybrodzki <w.miedzybrodzki@nowaera.pl>
2022-06-02 21:28:35 +0200

    chore: add config & bootstrap file for "kernel" tests

---
+++ tests/data/bootstrap.app.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Mmi Framework (https://github.com/milejko/mmi.git)
+ * 
+ * @link       https://github.com/milejko/mmi.git
+ * @copyright  Copyright (c) 2010-2017 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+
+define('BASE_PATH', __DIR__ . '/../../');
+require BASE_PATH . 'vendor/autoload.php';
+
