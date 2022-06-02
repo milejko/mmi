ce44b6370636b3e8b1210e89622095560bf14f68
Wojciech Miedzybrodzki <w.miedzybrodzki@nowaera.pl>
2022-06-02 21:32:25 +0200

    feat!: extract "router apply" to invokable service

---
+++ src/Mmi/Mvc/RouterApply.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Mmi\Mvc;
+
+use DI\Annotation\Inject;
+use Mmi\Http\Request;
+
+final class RouterApply
+{
+    private string $baseUrl;
+    private Router $router;
+
+    /** @Inject({"baseUrl" = "app.base.url"}) */
+    public function __construct(string $baseUrl, Router $router)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->router = $router;
+    }
+
+    public function __invoke(Request $request): void
+    {
+        // router apply (with baseUrl calculation)
+        $requestUri = $request->getServer()->requestUri;
+
+        if (strpos($requestUri, $this->baseUrl)) {
+            $requestUri = substr($requestUri, strlen($this->baseUrl));
+        }
+
+        $request->setParams($this->router->decodeUrl($requestUri));
+    }
+}
