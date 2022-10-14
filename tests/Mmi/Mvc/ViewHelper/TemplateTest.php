3b935abb22d352eb73a6f1b5e9249da1de8ed503
Mariusz Miłejko <mariusz@milejko.pl>
2022-10-14 10:14:46 +0200

    layout cache fix + stub->sandbox available (#53)

--- tests/Mmi/Mvc/ViewHelper/TemplateTest.php
+++ tests/Mmi/Mvc/ViewHelper/TemplateTest.php
@@ -21,9 +21,15 @@ class TemplateTest extends \PHPUnit\Framework\TestCase
     {
         $tpl = new Template(new View(TestApp::$di));
         $this->assertStringContainsString('/?module=test', $tpl->template('{@module=test@}'));
-        $this->assertEquals('<h1>It works!</h1>' . "\n" .
+        $this->assertEquals('<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">' . "\n" .
+            '<html><head>' . "\n" .
+            '<title>It works!</title>' . "\n" .
+            '</head><body>' . "\n" .
+            '<h1>It works!</h1>' . "\n" .
             '<p>This is the default web page for this server.</p>' . "\n" .
-            '<p>The web server software is running but no content has been added, yet.</p>', $tpl->template('{\'mmi/index/index\'}'));
+            '<p>The web server software is running but no content has been added, yet.</p>' . "\n" .
+            '</body></html>'
+            , $tpl->template('{\'mmi/index/index\'}'));
         $this->assertEquals('', $tpl->template('{\'inexistent/template\'}'));
         $this->assertEquals('<?php $this->test  =  1; ?><?php echo $this->test; ?>', $tpl->template('{$test = 1}{$test}'));
         $this->assertEquals('<?php $this->test  =  123; ?><?php echo $this->getFilter(\'truncate\')->setOptions(array(1))->filter($this->test); ?>', $tpl->template('{$test = 123}{$test|truncate:1}'));
