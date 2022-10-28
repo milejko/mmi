d4bf59e3a8e939fe00fab80ed439b583c243fc1d
Mariusz Milejko <mariusz@milejko.pl>
2022-10-28 19:53:26 +0200

    better test suite

---
+++ tests/data/test-query.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Mmi Framework (https://bitbucket.org/mariuszmilejko/mmicms/)
+ *
+ * @link       https://bitbucket.org/mariuszmilejko/mmicms/
+ * @copyright  Copyright (c) 2010-2015 Mariusz Miłejko (http://milejko.com)
+ * @license    http://milejko.com/new-bsd.txt New BSD License
+ */
+namespace Tests\Mmi\Orm;
+
+/**
+ * Testowe query
+ */
+class TestQuery extends \Mmi\Orm\Query
+{
+
+    protected $_tableName = 'test';
+}
