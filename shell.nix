433b89ad66e0a9e142f4c81bf2b2f62f30dc660a
Wojciech Miedzybrodzki <w.miedzybrodzki@nowaera.pl>
2022-11-23 14:36:53 +0100

    fix: do not pass null to \unserialize()
    mmi.ERROR: 8192: unserialize(): Passing null to parameter #1 ($data) of type string is deprecated[/vendor/mmi/mmi/src/Mmi/Cache/Cache.php (190)]

---
+++ shell.nix
@@ -0,0 +1,26 @@
+# simple environment for running / debugging tests using [Nix](https://nixos.org)
+# to use this file:
+# 1. install Nix: `sh <(curl -L https://nixos.org/nix/install) --no-daemon`
+# 2. Run command: `nix-shell`
+
+{ pkgs ? import <nixpkgs> {}}:
+
+let
+    myPhp = pkgs.php81.buildEnv {
+        extensions = ({ enabled, all }: enabled ++ [ all.xdebug] ++ [ all.apcu ]);
+        extraConfig = ''
+         xdebug.mode=debug
+        '';
+    };
+in
+pkgs.mkShell {
+    packages = [
+        myPhp
+        myPhp.packages.composer
+        myPhp.packages.psysh
+    ];
+
+    shellHook = ''
+      vendor/bin/phpunit --no-coverage
+    '';
+}
