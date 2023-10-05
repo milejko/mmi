6d8b47471ea85c0e56d0f5c12ad0e579d9695ce7
Mariusz Milejko <mariusz@milejko.pl>
2023-10-05 09:48:50 +0200

    PNG file support fix

--- src/Mmi/Image/Image.php
+++ src/Mmi/Image/Image.php
@@ -29,9 +29,11 @@ class Image
         if (\is_resource($input) || \is_object($input)) {
             return $input;
         }
-        $nullErrorHandler = set_error_handler(function() { return true; });
-        set_error_handler($nullErrorHandler);
-        error_reporting(0);
+        $nullErrorHandler = set_error_handler(function() {
+            restore_error_handler();
+            return true;
+        });
+        set_error_handler($nullErrorHandler, 0);
         //jeśli krótki content zakłada że to ścieżka pliku
         $resource = @imagecreatefromstring((strlen($input) < self::BINARY_MIN_LENGTH) ? file_get_contents($input) : $input);
         //konwersja do truecolor
